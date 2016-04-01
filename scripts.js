879427d4d3f3b3b8bae95e870e7f904aee10fcf3
Derek Conjar <derek@derekconjar.com>
2016-04-01 11:20:28 -0400

    Prevent contact form from auto-loading

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
 document.getElementsByClassName('button')[0].onclick = function () {
+  var zdy085w0z48315;(function(d, t) {
+    var s = d.createElement(t), options = {
+    'userName':'newadventure',
+    'formHash':'zdy085w0z48315',
+    'autoResize':true,
+    'height':'597',
+    'async':true,
+    'host':'wufoo.com',
+    'header':'show',
+    'ssl':true};
+    s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+    s.onload = s.onreadystatechange = function() {
+    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+    try { zdy085w0z48315 = new WufooForm();zdy085w0z48315.initialize(options);zdy085w0z48315.display(); } catch (e) {}};
+    var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+  })(document, 'script');
+
   document.getElementsByClassName('popup-disabled')[0].className = 'popup-enabled';
 };
 
